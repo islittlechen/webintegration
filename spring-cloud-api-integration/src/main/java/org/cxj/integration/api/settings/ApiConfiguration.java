d62af87366f1c80f26efb4280273ee2b6b7a18b9
littlechen <islittlechen@sina.cn>
2018-05-28 18:09:47 +0800

    server config refresh 测试

--- spring-cloud-api-integration/src/main/java/org/cxj/integration/api/settings/ApiConfiguration.java
+++ spring-cloud-api-integration/src/main/java/org/cxj/integration/api/settings/ApiConfiguration.java
@@ -1,10 +1,12 @@
 package org.cxj.integration.api.settings;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Configuration
+@RefreshScope
 @ConfigurationProperties(prefix = "setting")
 public class ApiConfiguration {
 
