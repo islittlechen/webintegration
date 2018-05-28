779eac5b120ac9639297f24ed65c81818cc7890d
littlechen <islittlechen@sina.cn>
2018-05-28 16:15:04 +0800

    redis cluster 测试

--- spring-cloud-api-integration/src/main/java/org/cxj/integration/api/controller/HelloController.java
+++ spring-cloud-api-integration/src/main/java/org/cxj/integration/api/controller/HelloController.java
@@ -1,17 +1,31 @@
 package org.cxj.integration.api.controller;
 
+import org.cxj.integration.api.settings.ApiConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${api.hello}")
     private String hello;
 
+
+    @Autowired
+    private ApiConfiguration apiConfiguration;
+
+
     @RequestMapping("/api/hello")
     public String echo(){
         return this.hello;
+    }
+
+    @RequestMapping("api/configInfo")
+    public String apiConfigInfo(){
+        return this.apiConfiguration.getApiName()+","+this.apiConfiguration.getMaxExec();
     }
 }
---
+++ spring-cloud-api-integration/src/main/java/org/cxj/integration/api/settings/ApiConfiguration.java
@@ -0,0 +1,30 @@
+package org.cxj.integration.api.settings;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ConfigurationProperties(prefix = "setting")
+public class ApiConfiguration {
+
+    private String apiName;
+
+    private int maxExec;
+
+    public String getApiName() {
+        return apiName;
+    }
+
+    public void setApiName(String apiName) {
+        this.apiName = apiName;
+    }
+
+    public int getMaxExec() {
+        return maxExec;
+    }
+
+    public void setMaxExec(int maxExec) {
+        this.maxExec = maxExec;
+    }
+}
