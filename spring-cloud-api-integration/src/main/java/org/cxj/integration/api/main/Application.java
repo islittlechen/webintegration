cf0a211e6c7f73c718708a7c7d51dafbddabdb85
littlechen <islittlechen@sina.cn>
2018-05-31 16:43:43 +0800

    eureka server,producer,consumer 测试

--- spring-cloud-api-integration/src/main/java/org/cxj/integration/api/main/Application.java
+++ spring-cloud-api-integration/src/main/java/org/cxj/integration/api/main/Application.java
@@ -2,11 +2,13 @@ package org.cxj.integration.api.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableHystrix
+@EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
 @ComponentScan({"org.cxj.integration.api"})
