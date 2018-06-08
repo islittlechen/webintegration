63b0e3456105a90b1cca9256792aa09d69768ce2
littlechen <islittlechen@sina.cn>
2018-06-08 17:58:06 +0800

    自定义spring start 使用Conditional注解测试

--- spring-cloud-bus-kafka/src/main/java/org/cxj/bus/kafka/controller/BusRefreshController.java
+++ spring-cloud-bus-kafka/src/main/java/org/cxj/bus/kafka/controller/BusRefreshController.java
@@ -1,10 +1,12 @@
 package org.cxj.bus.kafka.controller;
 
+import org.cxj.mystart.service.ExampleService;
 import org.cxj.mystart.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RefreshScope
@@ -17,8 +19,16 @@ public class BusRefreshController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ExampleService exampleService;
+
     @RequestMapping("/bus/test")
     public String test(){
         return this.apiName+userService.helloStarter();
+    }
+
+    @RequestMapping("/bus/example")
+    public String example(@RequestParam("word") String word){
+        return exampleService.wrap(word);
     }
 }
